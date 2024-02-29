6d9ccff5ad2f2c81c59b707fc0585fedfd2e7d4a
Gabriel Cavalcante Melo <Gabrielcvml@academicos.com>
2024-02-29 20:33:11 -0300

    Aula 02

---
+++ Aula02/App.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import Atividade1 from './src/components/Atividade1'
+
+const App = () => {
+  return (
+    <>
+      <Atividade1/>
+    </>
+  )
+}
+
+export default App
