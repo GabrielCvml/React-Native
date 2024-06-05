3e699a97aa9195c2e3143f26a61ebadff8e1df2f
GabriellC <103397042+GabrielCvml@users.noreply.github.com>
2024-06-05 11:13:13 -0300

    Atualizações finais

--- study-app/App.js
+++ study-app/App.js
@@ -4,7 +4,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { StudyCardsProvider } from './src/contexts/StudyCardsContext';
 import CardListScreen from './src/screens/CardListScreen';
 import CardEditScreen from './src/screens/CardEditScreen';
-import TaskDueSoonScreen from './src/screens/TaskDueSoonScreen';
+import TasksDueSoonScreen from './src/screens/TasksDueSoonScreen';
 
 const Stack = createStackNavigator();
 
@@ -15,7 +15,7 @@ const App = () => {
                 <Stack.Navigator initialRouteName="CardList">
                     <Stack.Screen name="CardList" component={CardListScreen} options={{ title: 'Study Cards' }} />
                     <Stack.Screen name="CardEdit" component={CardEditScreen} options={{ title: 'Editar Card' }} />
-                    <Stack.Screen name="TaskDoeSoon" component={TaskDueSoonScreen} options={{title: 'Tasks a vencer'}}/>
+                    <Stack.Screen name="TasksDueSoon" component={TasksDueSoonScreen} options={{title: 'Tasks a Vencer'}} />
                 </Stack.Navigator>
             </NavigationContainer>
         </StudyCardsProvider>
--- study-app/src/contexts/StudyCardsContext.js
+++ study-app/src/contexts/StudyCardsContext.js
@@ -1,153 +1,48 @@
-import { View, Text, Button, FlatList, TouchableOpacity, StyleSheet } from 'react-native'
-import React, { useContext } from 'react'
-import StudyCardsContext from '../contexts/StudyCardsContext'
+import React, { createContext, useState, useEffect } from 'react'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
-const CardListScreen = ({ navigation }) => {
+const StudyCardsContext = createContext()
 
-    const { cards, deleteCard } = useContext(StudyCardsContext)
+export const StudyCardsProvider = ({ children }) => {
+    const [cards, setCards] = useState([])
 
-    //Filtrar os cards por status
-    const inProgressCards = cards.filter(card => card.status === 'in_progress')
-    const concludedCards = cards.filter(card => card.status === 'done')
-    const backlogCards = cards.filter(card => card.status === 'backlog')
+    useEffect(() => {
+        loadCards()
+    }, [])
 
-    const today = new Date()
-    const dueSoonCards = cards.filter(card => {
-        const dueDate = new Date(card.dueDate)
-        const diffInDays = (dueDate - today) / (1000 * 60 * 60 * 24)
-        return diffInDays >= 0 && diffInDays <= 15
-    })
+    const loadCards = async () => {
+        //await AsyncStorage.clear();
+        //console.log('AsyncStorage was cleared successfully!');
+        const storedCards = await AsyncStorage.getItem('cards')
+        if (storedCards) setCards(JSON.parse(storedCards))
+    }
 
-    const renderCard = ({ item }) => (
-      <View style={styles.card}>
-        <Text style={styles.cardTitle}>{item.title}</Text>
-        <Text>Status: {item.status}</Text>
-        <Text>Data/Hora de Término: {new Date(item.dueDate).toLocaleString()}</Text>
-        <View>
-          <Button title='Editar' onPress={() => navigation.navigate('CardEdit', { id: item.id })} />
-          <Button title='Deletar' onPress={() => deleteCard(item.id)} color="#ff6347" />
-        </View>
+    const addCard = async (card) => {
+        const newCards = [...cards, { ...card, id: Date.now() }]
+        setCards(newCards)
+        await AsyncStorage.setItem('cards', JSON.stringify(newCards))
+    }
 
-      </View> 
+    const updateCard = async (id, updates) => {
+        const newCards = cards.map(card => card.id === id ? {
+            ...card, ...updates
+        } : card)
+        setCards(newCards)
+        await AsyncStorage.setItem('cards', JSON.stringify(newCards))
+    }
+
+    const deleteCard = async (id) => {
+        const newCards = cards.filter(card => card.id !== id)
+        setCards(newCards)
+        await AsyncStorage.setItem('cards', JSON.stringify(newCards))
+    }
+
+    return (
+        <StudyCardsContext.Provider value={{ cards, addCard, updateCard, deleteCard }}>
+            {children}
+        </StudyCardsContext.Provider>
     )
 
-  return (
-    <View style={styles.container}>
-      <TouchableOpacity style={styles.dueSoonButton} onPress={() => navigation.navigate('TasksDueSoon')}>
-        <Text style={styles.dueSoonButtonText}>Tasks a Vencer: {dueSoonCards.length}</Text>
-      </TouchableOpacity>
-      <Text style={styles.sectionTitle}>Em Progresso</Text>
-      <FlatList 
-        data={inProgressCards}
-        keyExtractor={item => item.id.toString()}
-        renderItem={renderCard}
-        horizontal={true}
-        showsHorizontalScrollIndicator={false}
-      />
-      <View style={styles.divider} />
-      <Text style={styles.sectionTitle}>Concluído</Text>
-      <FlatList 
-        data={concludedCards}
-        keyExtractor={item => item.id.toString()}
-        renderItem={renderCard}
-        horizontal={true}
-        showsHorizontalScrollIndicator={false}
-      />
-      <View style={styles.divider} />
-      <Text style={styles.sectionTitle}>Backlog</Text>
-      <FlatList 
-        data={backlogCards}
-        keyExtractor={item => item.id.toString()}
-        renderItem={renderCard}
-        horizontal={true}
-        showsHorizontalScrollIndicator={false}
-      />
-
-      <TouchableOpacity style={styles.addButton} onPress={() => navigation.navigate('CardEdit')}>
-        <Text style={styles.addButtonText}>+ Adicionar Novo Card</Text>
-      </TouchableOpacity>
-      
-    </View>
-  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-      flex: 1,
-      padding: 10,
-      backgroundColor: '#f9f9f9',
-  },
-  dueSoonButton: {
-      backgroundColor: '#ff4500',
-      padding: 10,
-      borderRadius: 10,
-      justifyContent: 'center',
-      alignItems: 'center',
-      marginBottom: 20,
-  },
-  dueSoonButtonText: {
-      color: '#ffffff',
-      fontSize: 16,
-      fontWeight: 'bold',
-  },
-  suggestButton: {
-      backgroundColor: '#4682b4',
-      padding: 10,
-      borderRadius: 10,
-      justifyContent: 'center',
-      alignItems: 'center',
-      marginBottom: 20,
-  },
-  suggestButtonText: {
-      color: '#ffffff',
-      fontSize: 16,
-      fontWeight: 'bold',
-  },
-  card: {
-      backgroundColor: '#ffffff',
-      padding: 20,
-      margin: 8,
-      borderRadius: 10,
-      shadowColor: '#000',
-      shadowOffset: { width: 0, height: 2 },
-      shadowOpacity: 0.1,
-      shadowRadius: 6,
-      elevation: 3,
-      minWidth: 200,
-  },
-  cardTitle: {
-      fontSize: 18,
-      fontWeight: 'bold',
-  },
-  buttons: {
-      flexDirection: 'row',
-      justifyContent: 'space-around',
-      marginTop: 10,
-  },
-  addButton: {
-      backgroundColor: '#4682b4',
-      padding: 15,
-      borderRadius: 10,
-      justifyContent: 'center',
-      alignItems: 'center',
-      marginTop: 10,
-  },
-  addButtonText: {
-      color: '#ffffff',
-      fontSize: 16,
-      fontWeight: 'bold',
-  },
-  sectionTitle: {
-      fontSize: 20,
-      fontWeight: 'bold',
-      marginTop: 20,
-      marginBottom: 10,
-  },
-  divider: {
-      borderBottomColor: '#cccccc',
-      borderBottomWidth: 1,
-      marginVertical: 10,
-  }
-});
-
-export default CardListScreen
+export default StudyCardsContext
